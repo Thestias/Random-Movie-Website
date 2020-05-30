b5e86901af2ccfa8943860b23b5a83221d9e8487
Conrado Franceschi <franceschiconrado@gmail.com>
2020-05-30 02:17:45 -0300

    Bug Fixes

--- apps/randomovie/views.py
+++ apps/randomovie/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Movie, UserFavoriteMovies
+from apps.randomovie.models import Movie, UserFavoriteMovies
 import random
 from django.contrib import messages
 
