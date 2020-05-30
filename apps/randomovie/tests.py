597086a40917aa08fc0cf0d01fbd9ba07551ec36
Conrado Franceschi <franceschiconrado@gmail.com>
2020-05-30 02:17:58 -0300

    New Tests

--- apps/randomovie/tests.py
+++ apps/randomovie/tests.py
@@ -1,43 +1,48 @@
 from django.test import TestCase
-from django.test import SimpleTestCase
 from django.urls import resolve, reverse
-from randomovie.views import homepage
-from randomovie.models import Movie
-from django.shortcuts import get_object_or_404
-
+from apps.randomovie.views import homepage, about, specific_movie
+from utils.create_model_objects_for_test import obj_movie_mod_create
 # Create your tests here.
 
 
-class HomePageTest(TestCase):
+class ViewMatchTest(TestCase):
 
     def setUp(self):
-        '''
-        When testing a empty database is created, this code below creates two objects in that database to perform tests.
-        '''
-        Movie.objects.create(movie_id=102899, imbd_id='tt0478970', movie_title='Ant-Man', genero='Adventure', original_language='en', overview='''
-            Armed with the astonishing ability to shri
-            nk in scale but increase in strength, master thief Scott Lang must embrace his i
-            nner-hero and help his mentor, Doctor Hank Pym, protect the secret behind his sp
-            ectacular Ant-Man suit from a new generation of towering threats. Against seemin
-            gly insurmountable obstacles, Pym and Lang must plan and pull off a heist that w
-            ill save the world.')
-            ''',
-                             poster_path='/fddtVEUozxvLmehR5yGZjem44pD.jpg', release_date='2015-07-14', budget='130000000', revenue='519311965',
-                             runtime='117', vote_average=7.1
-                             )
+        obj_movie_mod_create()
 
-        Movie.objects.create(movie_id=99999, imbd_id='tt3783958', movie_title='La La Land', genero='Drama', original_language='en', overview='''
-            Mia, an aspiring actress, serves lattes to movie stars in between auditions and Sebastian, a jazz musician, scrapes by playing cocktail party gigs in dingy bars, but as success mounts they are faced with decisions that begin to fray the fragile fabric of their love affair, and the dreams they worked so hard to maintain in each other threaten to rip them apart.')
-            ''',
-                             poster_path='//q0dsNZOuSKzAdZod9ohPXJ4GQUs.jpg', release_date='2016-11-29', budget='30000000', revenue='446486225',
-                             runtime='129', vote_average=7.9
-                             )
-
-    def test_url_resolves_to_homepage_view(self):
+    def test_homepage_view(self):
+        # The resolve() function finds the view that matches the given url
         find = resolve('/')
         self.assertEqual(find.func, homepage)
 
-    def test_url_status_code_to_ok(self):
+    def test_about_view(self):
+        find = resolve('/about/')
+        self.assertEqual(find.func, about)
+
+    def test_specific_movie_view(self):
+        find = resolve('/id=tt0478970/')
+        self.assertEqual(find.func, specific_movie)
+
+
+class PageStatusOkTest(TestCase):
+    '''
+        This Class Tests that all views return the status code 200.
+    '''
+
+    def setUp(self):
+        obj_movie_mod_create()
+
+    def test_homepage_status_code_to_ok(self):
         url = reverse('homepage')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_status_code_to_ok(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_specify_movie_status_code_to_ok(self):
+        url = reverse('specify_movie', args=['tt3783958'])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
