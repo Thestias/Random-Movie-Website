147a1d75bb56a2183c5c3114e75a07f279819fa7
Thestias <franceschiconrado@gmail.com>
2020-07-13 14:51:24 -0300

    Added Movies Model to Admin Page

--- apps/randomovie/admin.py
+++ apps/randomovie/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-# from .models import Movie
-# # Register your models here.
+from .models import Movie
+# Register your models here.
 
-# admin.site.register(Movie)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+	list_display = ('movie_id', 'imbd_id', 'movie_title', 'genero', 'original_language', 'release_date', 'runtime', 'vote_average', 'tagline',)
+	list_per_page = 15
+	list_filter = ('genero',)
