b1ee3f28c6ea73aca0526a36a971ab4447b74442
Thestias <franceschiconrado@gmail.com>
2020-07-22 21:58:38 -0300

    Added Tests

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from apps.users.views import register
+from django.contrib.auth import views as auth_views
+from django.urls import resolve, reverse
 # Create your tests here.
+
+
+class PageStatusOkTest(TestCase):
+    '''
+        This Class Tests that all views return the status code 200.
+    '''
+
+    def test_login_status_code_to_ok(self):
+        url = reverse('login')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_status_code_to_ok(self):
+        url = reverse('register')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
